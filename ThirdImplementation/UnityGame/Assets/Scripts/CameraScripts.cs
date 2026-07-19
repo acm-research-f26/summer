a12e53bf984414173108b833494e389c59f7513d
MahdMalik <mahdmahdmalik@gmail.com>
2026-07-19 17:25:23 -0500

    basic physics implemented now. might as well implement the actions and facts, then can work on the scasp part

--- ThirdImplementation/UnityGame/Assets/Scripts/CameraScripts.cs
+++ ThirdImplementation/UnityGame/Assets/Scripts/CameraScripts.cs
@@ -12,6 +12,6 @@ public class CameraScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1.5f, -10f);
     }
 }
--- ThirdImplementation/UnityGame/Assets/Scripts/PlayerScript.cs
+++ ThirdImplementation/UnityGame/Assets/Scripts/PlayerScript.cs
@@ -3,14 +3,45 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public BoxCollider2D tungCollider;
+    BoxCollider2D plrCollider;
+
+    public bool isJumping;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        plrCollider = GetComponent<BoxCollider2D>();
+        isJumping = false;
+
+        Physics2D.IgnoreCollision(plrCollider, tungCollider);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Keyboard.current.aKey.isPressed)
+        {
+            rb.linearVelocityX = -10;
+        }
+        else if(Keyboard.current.dKey.isPressed)
+        {
+            rb.linearVelocityX = 10;
+        }
+
+        if((Keyboard.current.spaceKey.isPressed || Keyboard.current.wKey.isPressed) && !isJumping)
+        {
+            isJumping = true;
+            rb.linearVelocityY = 50;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (contact.normal.y > 0.7f) isJumping = false;
+        }
     }
 }
